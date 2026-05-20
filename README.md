1769807f865e18c6e151b26ae24df2281218ca4c
Jefferson <jeffersonguerrero415@gmail.com>
2026-05-20 18:01:38 -0500

    feat: formulario signup

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # 02. Fundamentos de Angular
 ## Pagina principal
 <img src="assets/01-pagina-principal.png" width="600">
+
+# 05-formularios-reactivos-practica-a
+## Formulario con errores 
+<img src="assets/05-errores.png" width="600">
+
+
+
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 import { HomePage } from './features/home/pages/home-page/home-page';
 import { StudentDetailPage } from './features/students/pages/student-detail-page/student-detail-page';
 import { StudentPage } from './features/students/pages/student-page/student-page';
+import { SignupPage } from './features/signup/pages/signup-page/signup-page';
+import { LayoutsPage } from './features/layouts/pages/layouts-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'students', component: StudentPage },
   { path: 'students/:id', component: StudentDetailPage },
-  
-  {path: 'layouts',
-    loadComponent: () => import('./features/layouts/pages/layouts-page').then(m => m.LayoutsPage)
-  },
+  {path: 'layouts', component: LayoutsPage},
+  { path: 'signup', component: SignupPage },  
   //redireccionamiento
   { path: '**', redirectTo: '' },
 ];
