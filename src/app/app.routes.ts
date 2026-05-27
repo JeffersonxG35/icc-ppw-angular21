fd60838ea1b5dd47ae21426aaf290befb2c3c1b3
Jefferson <jeffersonguerrero415@gmail.com>
2026-05-26 22:44:43 -0500

    feat: registrar ruta simpsons/:id en app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,16 +6,21 @@ import { SignupPage } from './features/signup/pages/signup-page/signup-page';
 import { LayoutsPage } from './features/layouts/pages/layouts-page';
 import { ProfilePage } from './features/profile/pages/profile-page/profile-page';
 import ProjectConfigPage from './features/project/pages/project-config-page/project-config-page';
+import { UiComponentsPage } from './features/ui-components/pages/ui-components-page/ui-components-page';
+import { SimpsonsPage } from './features/simpsons/pages/simpsons-page/simpsons-page';
+import { SimpsonDetailPageComponent } from './features/simpsons/pages/simpson-detail-page/simpson-detail-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'students', component: StudentPage },
   { path: 'students/:id', component: StudentDetailPage },
-  {path: 'layouts', component: LayoutsPage},
-  { path: 'signup', component: SignupPage },  
+  { path: 'layouts', component: LayoutsPage },
+  { path: 'signup', component: SignupPage },
   { path: 'profile', component: ProfilePage },
   { path: 'project-config', component: ProjectConfigPage },
-
+  { path: 'ui-components', component: UiComponentsPage },
+  { path: 'simpsons', component: SimpsonsPage },
+  { path: 'simpsons/:id', component: SimpsonDetailPageComponent },
   //redireccionamiento
   { path: '**', redirectTo: '' },
 ];
